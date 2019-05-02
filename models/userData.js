b5c07517d42177ff40d7e0a0be19cb080cb776bf
Travis <travisformayor@users.noreply.github.com>
2019-05-02 14:50:40 -0700

    cleanup

--- models/userData.js
+++ models/userData.js
@@ -1,21 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema(
-	{
-		name: String,
-		username: {
-			type: String,
-			unique: true,
-		},
-		email: String,
-		fCity: String,
-		date: {
-			type: Date,
-			default: Date.now
-		}
-	}
-);
+const UserSchema = new Schema({
+  name: String,
+  username: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  favoriteCity: String,
+  userPic: String,
+  signupDate: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 const UserData = mongoose.model('User', UserSchema);
-module.exports = UserData;  
+
+module.exports = UserData;
